9841980166f1e2c09cdd2dc14376401b29db31cd
evanmpollack <50115376+evanmpollack@users.noreply.github.com>
2020-07-10 14:01:38 -0400

    Updated nav with correct names

--- inc/nav.php
+++ inc/nav.php
@@ -1,8 +1,8 @@
 			<nav class="menu">
 				<ul class="nav">
 					<li id="home" class="home"><a href="index.php">Home</a></li>
-					<li id="michael" class="michael"><a href="michael.php">Michael's Page</a></li>
-					<li id="eunlim" class="eunlim"><a href="eunlim.php">Eunlim's Page</a></li>
+					<li id="leah" class="leah"><a href="leah.php">Leah's Page</a></li>
+					<li id="evan" class="evan"><a href="evan.php">Evan's Page</a></li>
 					<li id="dan" class="dan"><a href="dan.php">Dan's Page</a></li>
 				</ul>
 			</nav>
