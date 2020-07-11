d0e9820ddddc760275f4fee84cc02e2c808ecf53
Evan Pollack <evanmpollack@gmail.com>
2020-07-11 19:53:03 -0400

    spacing/indent edits

--- evan.php
+++ evan.php
@@ -21,50 +21,52 @@
     <main>
         <article>
             <h2>A little about Evan</h2>
-             <img src="images/self.jpg" alt="me">
-                <section>
-                    <h3>Introduction</h3>
 
-                    <p>I am a student at the University of Rochester, which is located in Rochester, NY. My major is
-                        undecided, but I plan on minoring in computer science. I grew up in the Washington D.C. area. I
-                        have also been involved in extracurricular activities such as running competitively and
-                        snowboarding. In my free time, I enjoy activities such as hiking, biking, and kayaking.</p>
-                </section>
+            <img src="images/self.jpg" alt="me">
 
-                <section>
-                    <h3>Early Life</h3>
+            <section>
+                <h3>Introduction</h3>
 
-                    <p>I was born in Washington D.C. but have always lived in Maryland with my parents
-                        and two younger brothers, who are twins. During the first 10 years of my life, my family and I
-                        lived in Bethesda, MD. While living there, I attended Rosemary Hills Elementary
-                        School. In 2009, my family and I moved further out from D.C. to Sandy Spring, MD. After the move,
-                        I attended Barrie School and James Hubert Blake High School. During high school, my interest in
-                        STEM grew, leading me to explore many subjects. Out of the courses I took during those 4 years,
-                        AP Computer Science piqued my interest the most, inspiring me to pursue it in college.</p>
-                </section>
+                <p>I am a student at the University of Rochester, which is located in Rochester, NY. My major is
+                    undecided, but I plan on minoring in computer science. I grew up in the Washington D.C. area. I
+                    have also been involved in extracurricular activities such as running competitively and
+                    snowboarding. In my free time, I enjoy activities such as hiking, biking, and kayaking.</p>
+            </section>
 
-                <section>
-                    <h3>Relevant CS Coursework</h3>
+            <section>
+                <h3>Early Life</h3>
 
-                    <ul>
-                        <li>CSC 170: Intro to Web Development</li>
-                        <li>CSC 171: Intro to Computing Science</li>
-                        <li>CSC 172: Data Structures and Algorithms</li>
-                        <li>CSC 210: Web Programming</li>
-                    </ul>
-                </section>
+                <p>I was born in Washington D.C. but have always lived in Maryland with my parents
+                    and two younger brothers, who are twins. During the first 10 years of my life, my family and I
+                    lived in Bethesda, MD. While living there, I attended Rosemary Hills Elementary
+                    School. In 2009, my family and I moved further out from D.C. to Sandy Spring, MD. After the move,
+                    I attended Barrie School and James Hubert Blake High School. During high school, my interest in
+                    STEM grew, leading me to explore many subjects. Out of the courses I took during those 4 years,
+                    AP Computer Science piqued my interest the most, inspiring me to pursue it in college.</p>
+            </section>
 
-                <section>
-                    <h3>Extracurricular Activities</h3>
+            <section>
+                <h3>Relevant CS Coursework</h3>
 
-                    <ul>
-                        <li>Member of ΣΦΕ (2019-Present)</li>
-                        <li>Snowboarding (2012-Present)</li>
-                        <li>Cross Country and Track (2011-2019)</li>
-                    </ul>
-                </section>
-            </article>
-        </main>
+                <ul>
+                    <li>CSC 170: Intro to Web Development</li>
+                    <li>CSC 171: Intro to Computing Science</li>
+                    <li>CSC 172: Data Structures and Algorithms</li>
+                    <li>CSC 210: Web Programming</li>
+                </ul>
+            </section>
+
+            <section>
+                <h3>Extracurricular Activities</h3>
+
+                <ul>
+                    <li>Member of ΣΦΕ (2019-Present)</li>
+                    <li>Snowboarding (2012-Present)</li>
+                    <li>Cross Country and Track (2011-2019)</li>
+                </ul>
+            </section>
+        </article>
+    </main>
 
     <footer>
         <a href="http://csc174.org/lab01/epollack/" class="evan">Evan's Original Lab 1</a>
