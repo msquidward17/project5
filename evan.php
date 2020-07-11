fba022960aa1d4e65ce8e3b3c36623005906799a
Evan Pollack <evanmpollack@gmail.com>
2020-07-11 15:06:02 -0400

    Added content to my page and made a few css changes

--- evan.php
+++ evan.php
@@ -1,1 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<title>Evan Pollack | LA Project 2</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/navbar.css">
+	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&amp;family=Indie+Flower&amp;family=Pangolin&amp;family=Patrick+Hand&amp;family=Permanent+Marker&amp;display=swap" rel="stylesheet">
+</head>
 
+<body class="container">
+	<header class="intro">
+		<a href="index.php">
+			<h1>Evan Pollack</h1>
+		</a>
+	</header>
+
+	<?php include "inc/nav.php"; ?>
+
+    <main>
+        <article>
+            <h2>A little about Evan</h2>
+             <img src="images/self.jpg" alt="me">
+                <section>
+                    <h3>Introduction</h3>
+
+                    <p>I am a student at the University of Rochester, which is located in Rochester, NY. My major is
+                        undecided, but I plan on minoring in computer science. I grew up in the Washington D.C. area. I
+                        have been involved in activities such as running competitively and snowboarding.</p>
+                </section>
+
+                <section>
+                    <h3>Early Life</h3>
+
+                    <p>I was born in Washington D.C. but have always lived in Maryland with my parents
+                        and two younger brothers, who are twins. I attended Rosemary Hills Elementary
+                        School, Barrie School, and James Hubert Blake High School.</p>
+                </section>
+
+                <section>
+                    <h3>Relevant CS Coursework</h3>
+
+                    <ul>
+                        <li>CSC 170</li>
+                        <li>CSC 171</li>
+                        <li>CSC 172</li>
+                        <li>CSC 210</li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h3>Extracurricular Activities</h3>
+
+                    <ul>
+                        <li>Member of ΣΦΕ (2019-Present)</li>
+                        <li>Snowboarding (2012-Present)</li>
+                        <li>Cross Country and Track (2011-2019)</li>
+                    </ul>
+                </section>
+            </article>
+        </main>
+
+    <footer>
+        <a href="http://csc174.org/lab01/epollack/" class="evan">Evan's Original Lab 1</a>
+        <a href="index.php">Home</a>
+    </footer>
+
+    <?php include "inc/scripts.php"; ?>
+
+</body>
+</html>
