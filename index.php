cfadf4e107349aa2b5852480ab8199cc7bb415d6
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-07-11 21:47:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Home | LA Project 2</title>
 	<link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="css/navbar.css">
 	<link href="https://fonts.googleapis.com/css2?family=Fredoka+One&amp;family=Indie+Flower&amp;family=Pangolin&amp;family=Patrick+Hand&amp;family=Permanent+Marker&amp;display=swap" rel="stylesheet">
 </head>
+
 <body class="container">
 	<header class="intro">
 		<a href="index.php">
@@ -18,35 +20,36 @@
 	<?php include "inc/nav.php"; ?>
 
 	<main>
-	    <article>
-	        <h2>Our Group</h2>
+		<article>
+			<h2>Our Group</h2>
 
-		    <section class="article part1">
-			    <h3>Leah Goodwin</h3>
+			<section class="article part1">
+				<h3>Leah Goodwin</h3>
 
-			    <img src="images/me.jpg" alt="leah selfie">
+				<img src="images/me.jpg" alt="leah selfie">
 
-		        <p>Goal for Computer Science: </p>
+				<p>Goal for Computer Science: To pursue a career in the "creative side" of computer science, more specifically
+					Front-End Web Development and Video Game Development. </p>
 
-		        <div>
-		            <a href="leah.php" class="leah">Leah's Page</a>
+				<div>
+					<a href="leah.php" class="leah">Leah's Page</a>
 
-                    <a href="http://csc174.org/lab01/lgoodwi3/" class="leah">Leah's Lab 1</a>
-		        </div>
+					<a href="http://csc174.org/lab01/lgoodwi3/" class="leah">Leah's Lab 1</a>
+				</div>
 			</section>
 
 			<section class="article part2">
-			    <h3>Evan Pollack</h3>
+				<h3>Evan Pollack</h3>
 
 				<img src="images/self.jpg" alt="evan selfie">
 
 				<p>Goal for Computer Science: to have a job that incorporates, but isn't limited to, web development and
-				    computer science principles</p>
+					computer science principles</p>
 
 				<div>
-				    <a href="evan.php" class="evan">Evan's Page</a>
+					<a href="evan.php" class="evan">Evan's Page</a>
 
-                    <a href="http://csc174.org/lab01/epollack/" class="evan">Evan's Lab 1</a>
+					<a href="http://csc174.org/lab01/epollack/" class="evan">Evan's Lab 1</a>
 				</div>
 			</section>
 
@@ -58,9 +61,9 @@
 				<p>Goal for Computer Science: </p>
 
 				<div>
-				    <a href="dan.php" class="dan">Dan's Page</a>
+					<a href="dan.php" class="dan">Dan's Page</a>
 
-                    <a href="http://csc174.org/lab01/dzhang29/" class="dan">Dan's Lab 1</a>
+					<a href="http://csc174.org/lab01/dzhang29/" class="dan">Dan's Lab 1</a>
 				</div>
 			</section>
 		</article>
@@ -72,4 +75,5 @@
 
 	<?php include "inc/scripts.php"; ?>
 </body>
+
 </html>
