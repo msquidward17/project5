f6d8183e10e5c8373bde9f511e5e6177807ae6ca
jalhadab <67709677+jalhadab@users.noreply.github.com>
2020-07-19 22:05:38 +0300

    added a basic layout with some colors

--- leah.php
+++ leah.php
@@ -20,22 +20,21 @@
             </div>
         </header>
 
-        <main class="container">
-            <article>
-                <h2>A Little About Leah</h2>
-                <img src="images/me.jpg" alt="leah">
-                <section>
-                    <h3>Introduction</h3>
+        <main>
+            <article  class="container glayout">
+                
+                <section class = "intro boxy">
+                    <img class  = "propic" src="images/me.jpg" alt="leah">
                     <p>I'm currently a student at the University of Rochester, class of 2021. I'm majoring in Computer Science with a focus in Human Computer Interaction. I'm also minoring in International Relations. When I'm not busy building complex worlds, I like to write and draw in my spare time. My main goal is to pursue a career in the "creative side" of computer science, more specifically Front-End Web Development and Video Game Development.</p>
                 </section>
 
-                <section>
-                    <h3>Early Life</h3>
+                <section class = "elife boxy">
+                    <h2>Early Life</h2>
                     <p>Ever since I was young, I've had a natural affinity for writing, creative writing specifically. So I spent years creating complex worlds and figuring out how they would operate if it was the setting for a video game. It wasn't until I was introduced to the book series "The 39 Clues" that I realized how websites could be used as a medium for creativity. The series had a website dedicated to expanding the world presented inside the book in ways I had never seen before, with games, puzzles and an overall interactive website that engaged users. It was then that I started writing designs for websites for worlds I created, thus marking my start into being interested in visual media.</p>
                 </section>
 
-                <section>
-                    <h3>Relevant CS Coursework</h3>
+                <section class = "courses boxy">
+                    <h2>Relevant CS Coursework</h2>
                     <ul>
                         <li>CSC 170: Intro to Web Development</li>
                         <li>CSC 171: Intro to Computing Science</li>
@@ -46,8 +45,8 @@
                     </ul>
                 </section>
 
-                <section>
-                    <h3>Extracurricular Activities</h3>
+                <section class = "extrac boxy">
+                    <h2>Extracurricular Activities</h2>
                     <ul>
                         <li>Archery (2018-Present)</li>
                         <li>Table Tennis (2018-Present)</li>
