5cbfd14f1abb454cab4863241100ed14563e99c3
eunlimkim <ekim69@u.rochester.edu>
2020-08-03 00:13:48 -0400

    student list with css framework

--- inc/html-top.php
+++ inc/html-top.php
@@ -6,4 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>LA Project 4</title>
     <?php echo $customCSS; ?>
+    	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
 </head>
--- list.php
+++ list.php
@@ -1,77 +1,79 @@
 <?php $customCSS="<link rel='stylesheet' href='css/styles.css'>"; ?>
+<?php $customCSS="<link rel='stylesheet' href='css/override.css'>"; ?>
+
 <?php include "inc/html-top.php";?>
 
-	<body class="list">
-		<header >
-			<div class="container">
-				<a href="index.php">
-					<h1>Pursuing Web Development </h1>
-				</a>
-			</div>
-		</header>
 
-		<nav id="cssmenu">
-			<div class="container">
-				<ul>	
-					<li class="current"> Showcase </li>
-					<li><a href = "index.php" > Home </a></li>
-				</ul>
-			</div>
-		</nav>
 
+
+<body class="list">
+
+<nav class="navbar navbar-light bg-light">
+<span class="navbar-brand mb-0 h1 mx-auto"> 
+	<a href="index.php">
+	<h1>Pursuing Web Development </h1>
+	</a>  
+</span>
+</nav>
+
+	<nav class="navbar-right navbar-expand-lg navbar-light bg-light">
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Showcase <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+  
+    </ul>
+  </div>
+</nav>
 		<main>
-			<div class="container">
-				
-
-				<article>
-					<h2>Our Group</h2>
-
-					<section class="article part1">
-						<div class="left-flex">
-							<img src="images/me.jpg" alt="leah selfie">
-							<div class="goal">
-								<h3>Leah Goodwin</h3>
+			<div class="container-fluid">
+			<h2>Our Group</h2>
+			<div class="row">
+					<div class="col">
+					<img src="images/me.jpg" alt="leah selfie">
+					</div>
+					<div class="col">
+					<h3>Leah Goodwin</h3>
 								<p>Goal for Computer Science: To pursue a career in Front-End Web Development and Video Game Development.</p>
-							</div>
-						</div>
+					</div>
+					<div class="col">
+					<a href="leah.php" class="leah">Leah's Page</a>
+					</div>
+				</div>
+			
 
-							<div class="links">
-								<a href="leah.php" class="leah">Leah's Page</a>
-
-								<a href="http://csc174.org/lab01/lgoodwi3/" class="leah">Leah's Lab 1</a>
-							</div>
-					</section>
-
-					<section class="article part2">
-						<div class="left-flex">
-							<img src="images/self.png" alt="evan selfie">
-							<div class='goal'>
-								<h3>Evan Pollack</h3>
-								<p>Goal for Computer Science: To have a job that incorporates, but isn't limited to, web development and computer science principles.</p>
-							</div>
-						</div>
-							<div class="links">
-								<a href="evan.php" class="evan">Evan's Page</a>
-
-								<a href="http://csc174.org/lab01/epollack/" class="evan">Evan's Lab 1</a>
-							</div>
-					</section>
-
-					<section class="article part3">
-						<div class="left-flex">
-							<img src="images/girl.png" alt="girl">
-							<div class='dansgoal'>
-								<h3>Dan Zhang</h3>
-								<p>Goal for Computer Science: To pursue a career in Front-End Web Development.</p>
-							</div>
-						</div>
-							<div class="links">
-								<a href="dan.php" class="dan">Dan's Page</a>
-
-								<a href="http://csc174.org/lab01/dzhang29/" class="dan">Dan's Lab 1</a>
-							</div>
-					</section>
-				</article>
+			<div class="row">
+					<div class="col">
+					<img src="images/self.png" alt="evan selfie">
+					</div>
+					<div class="col">
+					
+					<h3>Evan Pollack</h3>
+					<p>Goal for Computer Science: To have a job that incorporates, but isn't limited to, web development and computer science principles.</p>
+					
+					</div>
+					<div class="col">
+					<a href="evan.php" class="evan">Evan's Page</a>	
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+					<img src="images/girl.png" alt="girl">
+					</div>
+					<div class="col">
+					<h3>Dan Zhang</h3>
+					<p>Goal for Computer Science: To pursue a career in Front-End Web Development.</p>
+					
+					</div>
+					<div class="col">
+					<a href="dan.php" class="dan">Dan's Page</a>
+					</div>
+				</div>
 			</div>
 		</main>
 
