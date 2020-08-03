5440260354de51ff79925e2d0ff881930bcf65b8
jkat718 <jkat718@users.noreply.github.com>
2020-08-03 14:48:18 -0400

    Whitespace Cleanup

--- db_view.php
+++ db_view.php
@@ -1,89 +1,80 @@
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Student Websites</title>
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/override.css">
-</head>
-<body>
+<?php $customCSS="<link rel='stylesheet' href='css/styles.css'>"; ?>
+<?php $use_bootstrap=true; ?>
+
+<?php include "inc/html-top.php";?>
+
+	<body>
 <?php
-// connect to the database
-include('inc/db_connect.php');
-
-// get results from database
-$result = mysqli_query($connection, "SELECT * FROM students");
-?>
-
-<nav class="navbar navbar-light">
-<span class="navbar-brand mb-0 h1 text-white ml-5 pl-5"> 
+	// connect to the database
+	include('inc/db_connect.php');
 	
-	Student List
-	 
-</span>
-</nav>
-
-	<nav class="navbar-expand-lg navbar-light pt-4 pr-3 mr-5 pr-5">
-  <div class="collapse navbar-collapse" id="navbarNav">
-  <ul class="nav navbar-nav navbar-right">
-      <li><a href="index.php" class="home">HOME</a></li>
-      <li><a href="#" class="current">SHOWCASE</a></li>
-    </ul>
-  </div>
-</nav>
-
-
-<main>
-
-<h2 class="line pl-5">Our Group</h2>
-<?php
-// loop through results of database query, displaying them in the table
-while($row = mysqli_fetch_array( $result )) {
+	// get results from database
+	$result = mysqli_query($connection, "SELECT * FROM students");
 ?>
-<div class="container-fluid pl-4 pr-4 kim">
+
+		<nav class="navbar navbar-light">
+			<span class="navbar-brand mb-0 h1 text-white ml-5 pl-5"> 
+				Pursuing Web Development
+			</span>
+		</nav>
+		
+		<nav class="navbar-expand-lg navbar-light pt-4 pr-3 mr-5 pr-5">
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="index.php" class="home">HOME</a></li>
+					<li><a href="#" class="current">SHOWCASE</a></li>
+				</ul>
+			</div>
+		</nav>
+		
+		<main>
+			<div class="container-fluid pl-4 pr-4 kim">
+				<h2 class="line">Our Group</h2>
+				
+<?php
+	// loop through results of database query, displaying them in the table
+	while($row = mysqli_fetch_array( $result )) {
+?>
 				<div class="row pb-4 pt-3">
 					<div class="col text-left ml-4">
-          <?php echo 'image goes here' ?>
+						<img src="<?php echo $row['img']; ?>" alt="image for <?php echo $row['firstname']; ?>">
 					</div>
-
-					<div class="col-7 text-left">
-					<h3><?php echo $row['firstname']; ?> <?php echo $row['lastname']; ?></h3>
-					<p><?php echo $row['description']; ?></p>
 					
+					<div class="col-7 text-left">
+						<h3><?php echo $row['firstname']; ?> <?php echo $row['lastname']; ?></h3>
+						<p><?php echo $row['description']; ?></p>
 					</div>
-
+					
 					<div class="col pt-4">
-					<a href="db_edit.php?id=<?php echo $row['id']; ?>">Edit</a>
-					<a onclick="return confirm('Are you sure you want to delete?')" href="delete.php?id=46">Delete</a>
-					<a href="evan.php" class="evan"><?php echo $row['link']; ?></a>
+						<a href="db_edit.php?id=<?php echo $row['id']; ?>">Edit</a>
+						<a onclick="return confirm('Are you sure you want to delete: <?php echo $row['firstname']; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
+						<a href="<?php echo $row['link']; ?>.php" class="page-link"><?php echo $row['firstname']; ?>'s Page</a>
 					</div>
+				</div>
+<?php
+	// close the loop
+	}
+?>
+				<div class="row pb-5 mb-5 d-flex justify-content-center">
+					<a class="dan" href="db_new.php">Add New Student</a>
 				</div>
 			</div>
-      <?php
-// close the loop
-}
-?>
-<div class="row pb-5 mb-5 d-flex justify-content-center">
-				<a class="dan" href="db_new.php">Add New Student</a>
-				</div>
-        </div>
 		</main>
+
 <!-- Footer -->
-<footer class="page-footer font-small blue footer fixed-bottom footer-light footer-shadow">
+		<footer class="page-footer font-small blue footer fixed-bottom footer-light footer-shadow">
 
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">Project 4 - Los Angeles
-  </div>
-  <!-- Copyright -->
-
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
-</html>
+			<!-- Copyright -->
+			<div class="footer-copyright text-center py-3">
+				Project 4 - Los Angeles
+			</div>
+			<!-- Copyright -->
+		</footer>
+		
+<?php include "inc/scripts.php"; ?>
 <?php
-  mysqli_free_result($result);
-  mysqli_close($connection);
+	mysqli_free_result($result);
+	mysqli_close($connection);
 ?>
+	</body>
+</html>
--- inc/renderform.php
+++ inc/renderform.php
@@ -4,16 +4,18 @@ function renderForm($id, $firstname, $lastname, $description, $img, $link, $erro
 ?>
 <!doctype html>
 <html>
-<head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Student list</title>
-	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-	<link rel="stylesheet" href="css/add_student.css">
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<title>LA Project 4</title>
+		
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		<link rel="stylesheet" href="css/add_student.css">
+	</head>
+	<body>
 <?php
 // if there are any errors, display them
 if ($error != '') {
@@ -21,59 +23,60 @@ if ($error != '') {
 }
 ?>
 
-<div class="container">
-        <div class="row centered-form">
-		<div class="add-form">
-        	<div class="panel panel-default">
-        		<div class="panel-heading">
-			    		<h3 class="panel-title">Add New Student</h3>
-			 			</div>
-			 			<div class="panel-body">
-						 <form action="" method="post">
-						 	<input type="hidden" name="id" value="<?php echo $id; ?>">
-			    			<div class="row">
-			    				<div class="col-xs-6 col-sm-6 col-md-6">
-			    					<div class="form-group">
-										<input type="text" id="firstname" name="firstname" value="<?php echo $firstname; ?>"  placeholder="First Name"><br>
-			    					</div>
-			    				</div>
-			    				<div class="col-xs-6 col-sm-6 col-md-6">
-			    					<div class="form-group">
-										<input type="text" id="lastname" name="lastname" value="<?php echo $lastname; ?>" placeholder="Last Name"><br>
-			    					</div>
-			    				</div>
-			    			</div>
+		<div class="container">
+			<div class="row centered-form">
+				<div class="add-form">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Add New Student</h3>
+						</div>
+						<div class="panel-body">
+							<form action="" method="post">
+								<input type="hidden" name="id" value="<?php echo $id; ?>">
+								<div class="row">
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="text" id="firstname" name="firstname" value="<?php echo $firstname; ?>"  placeholder="First Name"><br>
+										</div>
+									</div>
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="text" id="lastname" name="lastname" value="<?php echo $lastname; ?>" placeholder="Last Name"><br>
+										</div>
+									</div>
+								</div>
 
-			    			<div class="form-group">
-								<input type="file" id="img" name="img" value="<?php echo $img; ?>"><br>
-			    			</div>
+								<div class="form-group">
+									<input type="file" id="img" name="img" value="<?php echo $img; ?>"><br>
+								</div>
 
-			    			<div class="row">
-			    				<div class="col-xs-6 col-sm-6 col-md-6">
-			    					<div class="form-group">
-										<input type="text" id="description" name="description" value="<?php echo $description; ?>" placeholder="Description"><br>
-			    					</div>
-			    				</div>
-			    				<div class="col-xs-6 col-sm-6 col-md-6">
-			    					<div class="form-group">
-										<input type="texlinkt" id="link" name="link" value="<?php echo $link; ?>" placeholder="Link"><br>
-			    					</div>
-			    				</div>
-			    			</div>
-			    			
-			    			<input type="submit" name="submit" value="Submit" class="btn btn-info btn-block">
-			    		    <div id="formFooter">
-     						 <a class="underlineHover" href="db_view.php">Cancel</a>
-   							 </div>
-			    		</form>
-			    	</div>
-	    		</div>
-    		</div>        
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
+								<div class="row">
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="text" id="description" name="description" value="<?php echo $description; ?>" placeholder="Description"><br>
+										</div>
+									</div>
+									<div class="col-xs-6 col-sm-6 col-md-6">
+										<div class="form-group">
+											<input type="texlinkt" id="link" name="link" value="<?php echo $link; ?>" placeholder="Link"><br>
+										</div>
+									</div>
+								</div>
+								
+								<input type="submit" name="submit" value="Submit" class="btn btn-info btn-block">
+								<div id="formFooter">
+									<a class="underlineHover" href="db_view.php">Cancel</a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	</body>
 </html>
 <?php
 }
