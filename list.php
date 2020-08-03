a8c24d2ac5b96a8b77eacc4fabdd8928a41e0c53
jkat718 <jkat718@users.noreply.github.com>
2020-08-03 14:52:02 -0400

    Reverted list.php
    Reverted list.php to be exactly as it was in Project 3, will focus on updating db_view.php to have correct functionality and appearance.

--- list.php
+++ list.php
@@ -1,80 +1,87 @@
 <?php $customCSS="<link rel='stylesheet' href='css/styles.css'>"; ?>
-<?php $customCSS="<link rel='stylesheet' href='css/override.css'>"; ?>
-
 <?php include "inc/html-top.php";?>
 
-<body>
-<?php
-// connect to the database
-include('inc/db_connect.php');
+	<body class="list">
+		<header >
+			<div class="container">
+				<a href="index.php">
+					<h1>Pursuing Web Development </h1>
+				</a>
+			</div>
+		</header>
 
-// get results from database
-$result = mysqli_query($connection, "SELECT * FROM students");
-?>
+		<nav id="cssmenu">
+			<div class="container">
+				<ul>	
+					<li class="current"> Showcase </li>
+					<li><a href = "index.php" > Home </a></li>
+				</ul>
+			</div>
+		</nav>
 
-<nav class="navbar navbar-light">
-<span class="navbar-brand mb-0 h1 text-white ml-5 pl-5"> 
-	
-	Pursuing Web Development
-	 
-</span>
-</nav>
-
-	<nav class="navbar-expand-lg navbar-light pt-4 pr-3 mr-5 pr-5">
-  <div class="collapse navbar-collapse" id="navbarNav">
-  <ul class="nav navbar-nav navbar-right">
-      <li><a href="index.php" class="home">HOME</a></li>
-      <li><a href="#" class="current">SHOWCASE</a></li>
-    </ul>
-  </div>
-</nav>
 		<main>
-			<div class="container-fluid pl-4 pr-4 kim">
-			<h2 class="line">Our Group</h2>
-			
-<?php
-// loop through results of database query, displaying them in the table
-while($row = mysqli_fetch_array( $result )) {
-?>
-				<div class="row pb-4 pt-3">
-					<div class="col text-left ml-4">
-					<img src="<?php echo $row['img']; ?>" alt="image for <?php echo $row['firstname']; ?>">
-					</div>
+			<div class="container">
+				
 
-					<div class="col-7 text-left">
-					<h3><?php echo $row['firstname']; ?> <?php echo $row['lastname']; ?></h3>
-								<p><?php echo $row['description']; ?></p>
-					</div>
-					
-					<div class="col pt-4">
-					<a href="db_edit.php?id=<?php echo $row['id']; ?>">Edit</a>
-					<a onclick="return confirm('Are you sure you want to delete: <?php echo $row['firstname']; ?>?')" href="delete.php?id=<?php echo $row['id']; ?>">Delete</a>
-					<a href="<?php echo $row['link']; ?>.php" class="page-link"><?php echo $row['firstname']; ?>'s Page</a>
-					</div>
-				</div>
-<?php
-// close the loop
-}
-?>				
-				<div class="row pb-5 mb-5 d-flex justify-content-center">
-					<a class="dan" href="db_new.php">Add New Student</a>
-				</div>
+				<article>
+					<h2>Our Group</h2>
+
+					<section class="article part1">
+						<div class="left-flex">
+							<img src="images/me.jpg" alt="leah selfie">
+							<div class="goal">
+								<h3>Leah Goodwin</h3>
+								<p>Goal for Computer Science: To pursue a career in Front-End Web Development and Video Game Development.</p>
+							</div>
+						</div>
+
+							<div class="links">
+								<a href="leah.php" class="leah">Leah's Page</a>
+
+								<a href="http://csc174.org/lab01/lgoodwi3/" class="leah">Leah's Lab 1</a>
+							</div>
+					</section>
+
+					<section class="article part2">
+						<div class="left-flex">
+							<img src="images/self.png" alt="evan selfie">
+							<div class='goal'>
+								<h3>Evan Pollack</h3>
+								<p>Goal for Computer Science: To have a job that incorporates, but isn't limited to, web development and computer science principles.</p>
+							</div>
+						</div>
+							<div class="links">
+								<a href="evan.php" class="evan">Evan's Page</a>
+
+								<a href="http://csc174.org/lab01/epollack/" class="evan">Evan's Lab 1</a>
+							</div>
+					</section>
+
+					<section class="article part3">
+						<div class="left-flex">
+							<img src="images/girl.png" alt="girl">
+							<div class='dansgoal'>
+								<h3>Dan Zhang</h3>
+								<p>Goal for Computer Science: To pursue a career in Front-End Web Development.</p>
+							</div>
+						</div>
+							<div class="links">
+								<a href="dan.php" class="dan">Dan's Page</a>
+
+								<a href="http://csc174.org/lab01/dzhang29/" class="dan">Dan's Lab 1</a>
+							</div>
+					</section>
+				</article>
 			</div>
 		</main>
 
-<!-- Footer -->
-<footer class="page-footer font-small blue footer fixed-bottom footer-light footer-shadow">
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">Project 4 - Los Angeles
-  </div>
-  <!-- Copyright -->
+		<footer>
+			<div class="container">
+				<p>Project 3 - Los Angeles</p>
+			</div>
+		</footer>
 
 		<?php include "inc/scripts.php"; ?>
-		<?php
-  mysqli_free_result($result);
-  mysqli_close($connection);
-?>
 	</body>
 
 </html>
