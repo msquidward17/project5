52553a26575c6ea8647ebef3d43b12600fde36df
jkat718 <jkat718@users.noreply.github.com>
2020-08-03 13:01:59 -0400

    Set bootstrap styling to opt-in

--- inc/html-top.php
+++ inc/html-top.php
@@ -4,7 +4,17 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk' crossorigin='anonymous'>
+    
     <title>LA Project 4</title>
+	
+<?php
+	//add bootstrap styling & override styling if enabled (should only be used for newly-created pages)
+	if ($use_bootstrap) {
+?>
+	<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' integrity='sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk' crossorigin='anonymous'>
+	<link rel='stylesheet' href='css/override.css'>
+<?php
+	}
+?>
     <?php echo $customCSS; ?>
 </head>
