74ebca1d4ab778c67fae59b5a19f3eb87bf20c15
msquidward17 <67696489+msquidward17@users.noreply.github.com>
2020-08-06 12:37:10 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -26,6 +26,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+		<a href="index.php" class="btn btn-primary">Home page</a>
     </p>
 </body>
 </html>
